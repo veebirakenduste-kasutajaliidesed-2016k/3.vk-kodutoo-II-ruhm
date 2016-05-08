cd8b0740156cab1b61c35aba1f10425f28e21ab9
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2016-05-08 21:36:31 +0300

    eemaldatud üleliigne fail

--- upload.php
+++ upload.php
@@ -1,4 +1,4 @@
-<form action="uploadimage.php" method="post" enctype="multipart/form-data" style="">
+<form action="uploadimage.php" method="get" enctype="multipart/form-data" style="">
     Pildi laadimiseks vajuta kastile või tiri pilt kasti!
     <div style="border: 1px solid #000; border-radius: 10px; width: 100%; min-height: 400px; ">
       <input style="width: 100%; min-height: 400px; " type="file" name="fileToUpload" id="fileToUpload">
