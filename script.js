6711163b22e398b6c2901469bc8ab61424ed7be6
Kevin Münter <munterke@greeny.cs.tlu.ee>
2016-05-11 10:30:31 +0300

    Cats load now

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 
 $(document).ready(function(){
-    $('#boxMultibar').multibar();
+
 
     $("input[name=load]").click(function(){
         $.ajax({
               url: 'images.json',
               dataType: "json",
-
-              error: function(err){
-                console.log(err);
-              },
               success: function(array){
-                console.log(array);
-                
+                $('#boxMultibar').multibar();
+                $("#button").hide();
+
+
                 $.each( array, function( key, img ) {
-                  //append
+
                   var imgHtml = $('<img src="'+img.url+'">');
                   $("body").append(imgHtml);
                 });
