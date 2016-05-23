d64f348a8af2342f0732df354f2beec2f8d939da
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2016-05-23 18:23:30 +0300

    Üritasin juurde teha

--- flickr.js
+++ flickr.js
@@ -1,6 +1,6 @@
-var apiurl,myresult,apiurl_size,selected_size,popularurl;
+var apiurl,myresult,apiurl_size,selected_size,tagurl;
 apiurl = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=94268cb7e8c14262f02a71d8bc2e59b1&per_page=10&format=json&nojsoncallback=1";
-popularurl= "https://api.flickr.com/services/rest/?method=flickr.stats.getPopularPhotos&api_key=94268cb7e8c14262f02a71d8bc2e59b1&per_page=10&format=json&nojsoncallback=1";
+tagurl= "https://api.flickr.com/services/rest/?method=flickr.places.tagsForPlace&api_key=94268cb7e8c14262f02a71d8bc2e59b1&woe_id=845743&place_id=12&format=json&nojsoncallback=1&auth_token=72157668018662670-9f780da99c284157&api_sig=77b77f69e1748e1e6820cdb3f026479";
 $(document).ready(function(){
 $("#sq").click(function(){
 selected_size=75;
@@ -35,7 +35,23 @@ $(document).ready(function(){
 $('#button').click(function(){
 $.getJSON(apiurl,function(json) {
 $.each(json.photos.photo,function(i,myresult){
-apiurl_size = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=ca370d51a054836007519a00ff4ce59e&photo_id="+myresult.id+"&format=json&nojsoncallback=1";
+apiurl_size = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=94268cb7e8c14262f02a71d8bc2e59b1&photo_id="+myresult.id+"&format=json&nojsoncallback=1";
+$.getJSON(apiurl_size,function(size){
+$.each(size.sizes.size,function(i,myresult_size){
+if(myresult_size.width==selected_size){
+$("#results").append('<p><a href="'+myresult_size.url+'" target="_blank"><img src="'+myresult_size.source+'"/></a></p>');
+}
+})
+})
+});
+});
+});
+});
+$(document).ready(function(){
+$('#button1').click(function(){
+$.getJSON(tagurl,function(json) {
+$.each(json.tags.tag,function(i,myresult){
+apiurl_size = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=94268cb7e8c14262f02a71d8bc2e59b1&tag_id="+myresult.id+"&format=json&nojsoncallback=1";
 $.getJSON(apiurl_size,function(size){
 $.each(size.sizes.size,function(i,myresult_size){
 if(myresult_size.width==selected_size){
